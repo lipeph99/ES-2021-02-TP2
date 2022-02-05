2c8e36852c29d97bc9bf78ad83843dbd5104520b
lipeph99 <lipeph99@gmail.com>
2022-02-05 17:51:36 -0300

    Commit 10

--- Rental.java
+++ Rental.java
@@ -21,12 +21,6 @@ public class Rental {
     }
 
     public int getFrequentRenterPoints() {
-        // add frequent renter points
-        int frequentRenterPoints = 1;
-        // add bonus for a two day new release rental
-        if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                this.getDaysRented() > 1)
-            frequentRenterPoints++;
-        return frequentRenterPoints;
+        return _movie.getFrequentRenterPoints(_daysRented);
     }
 }
